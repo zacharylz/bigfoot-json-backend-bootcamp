fce37906af2a2888b4f025a1d76d754c215f691c
FoongL <leung.foong@gmail.com>
2022-07-08 16:33:25 +0800

    fixed up env file and modules

--- utils.js
+++ utils.js
@@ -1,8 +1,13 @@
-import { readFile } from "fs/promises";
+const { readFile } = require('fs/promises')
 
-export const getSightings = async () => {
+const getSightings = async () => {
   // Retrieve sightings.json file contents as string
   const sightings = await readFile("./sightings.json", "utf8");
   // Return sightings as an object
   return JSON.parse(sightings);
 };
+
+
+module.exports = {
+  getSightings
+}
