9daebb17938be492f6130333889122691077b2cb
zacharylz <zacharylz@outlook.sg>
2023-10-22 23:25:34 +0800

    feat: routes for all and specific sightings

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+// const express = require("express");
+// const { getSightings } = require("./utils.js");
+// require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import { getSightings, getSpecificSighting } from "./utils.js";
+import "dotenv/config";
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
+  res.json(sightings);
+});
+
+app.get("/sightings/:report_number", async (req, res) => {
+  const sightings = await getSpecificSighting(req.params.report_number);
   res.json(sightings);
 });
 
--- utils.js
+++ utils.js
@@ -1,13 +1,26 @@
-const { readFile } = require('fs/promises')
+// const { readFile } = require("fs/promises");
+import { readFile } from "fs/promises";
 
-const getSightings = async () => {
+export const getSightings = async () => {
   // Retrieve sightings.json file contents as string
   const sightings = await readFile("./sightings.json", "utf8");
   // Return sightings as an object
   return JSON.parse(sightings);
 };
 
+export const getSpecificSighting = async (report_number) => {
+  // Retrieve sightings.json file contents as string
+  const sightingsString = await readFile("./sightings.json", "utf8");
+  // Return sightings as an object
+  const sightings = JSON.parse(sightingsString);
+  for (let sighting of sightings) {
+    if (sighting.REPORT_NUMBER == report_number) {
+      return sighting;
+    }
+  }
+  return "Report not found";
+};
 
-module.exports = {
-  getSightings
-}
+// module.exports = {
+//   getSightings
+// }
